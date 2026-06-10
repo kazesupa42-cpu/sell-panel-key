70b1f1e907e314fdd243a899a3b6a33e67b2d7b2
kazesupa42-cpu <kazesupa42@gmail.com>
2026-06-10 23:42:14 +0800

    Update sell_inj.py

--- sell_inj.py
+++ sell_inj.py
@@ -11,7 +11,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 OWNER_ID = int(os.environ.get("OWNER_ID"))
-PANEL_URL = "https://codm-injector-panel-8oa2.onrender.com"
+PANEL_URL = "https://codm-injector-panel-4ewn.onrender.com"
 
 # ======================
 # KEEP ALIVE SERVER
